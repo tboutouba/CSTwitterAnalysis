2d85234ab18a2e753a8e0dc41c03d0183a010a1f
tboutouba <taha.boutouba@student-cs.fr>
2018-11-14 10:38:12 +0100

    minor update

--- twitter_collect/twitter_collection_setup.py
+++ twitter_collect/twitter_collection_setup.py
@@ -1,9 +1,24 @@
 import tweepy
+# We import our access keys:
 import credentials
 
-auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
-auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_SECRET)
-api = tweepy.API(auth)
+def twitter_setup():
+    """
+    Utility function to setup the Twitter's API
+    with an access keys provided in a file credentials.py
+    :return: the authentified API
+    """
+    # Authentication and access using keys:
+    auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
+    auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_SECRET)
 
-status = "Testing!"
-api.update_status(status=status)
+    # Return API with authentication:
+    api = tweepy.API(auth)
+    return api
+
+def collect():
+    connexion = twitter_setup()
+    tweets = connexion.search("Emmanuel Macron",language="french",rpp=100)
+    for tweet in tweets:
+        print(tweet.text)
+
